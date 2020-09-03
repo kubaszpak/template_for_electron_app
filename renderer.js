e405cf93c90b7f96a3f2dc8344c426216ccedfa2
kubaszpak <jakub.szpak.it@gmail.com>
2020-09-03 20:12:00 +0200

    Initial commit

--- renderer.js
+++ renderer.js
@@ -4,3 +4,55 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const remote = require("electron").remote;
+const { ipcRenderer } = require("electron");
+
+const win = remote.getCurrentWindow(); /* Note this is different to the
+html global `window` variable */
+
+// When document has loaded, initialise
+document.onreadystatechange = (event) => {
+  if (document.readyState == "complete") {
+    handleWindowControls();
+  }
+};
+
+window.onbeforeunload = (event) => {
+  /* If window is reloaded, remove win event listeners
+    (DOM element listeners get auto garbage collected but not
+    Electron win listeners as the win is not dereferenced unless closed) */
+  win.removeAllListeners();
+};
+
+function handleWindowControls() {
+  // Make minimise/maximise/restore/close buttons work when they are clicked
+  document.getElementById("min-button").addEventListener("click", (event) => {
+    win.minimize();
+  });
+
+  document.getElementById("max-button").addEventListener("click", (event) => {
+    if (!win.isMaximized()) {
+      win.maximize();
+    } else {
+      win.unmaximize();
+    }
+  });
+
+  document.getElementById("close-button").addEventListener("click", (event) => {
+    win.close();
+  });
+}
+
+// Toggle maximise/restore buttons when maximisation/unmaximisation occurs
+toggleMaxRestoreButtons();
+win.on("maximize", toggleMaxRestoreButtons);
+win.on("unmaximize", toggleMaxRestoreButtons);
+
+function toggleMaxRestoreButtons() {
+  if (win.isMaximized()) {
+    document.body.classList.add("maximized");
+  } else {
+    document.body.classList.remove("maximized");
+  }
+}
